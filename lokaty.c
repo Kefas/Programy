84bf3eb4a50a27688c25504806924e0be9f0d4dc
Piotr <piotr@piotrkonsek@gmail.com>
2012-11-21 23:27:15 +0100

    Lokaty

---
+++ lokaty.c
@@ -0,0 +1,34 @@
+int main(void)
+{
+ char nazwiska[ROZMIAR][30];
+ char tytuly[5][20] = {"Nazwisko" , "Wielkosc lokaty", "Dzien", "Miesiac", "Rok"};
+ int i, tablica[5][ROZMIAR];
+
+ for(i=0; i<ROZMIAR; i++)
+  {
+  printf("\nPodaj mi %d. nazwisko", i);
+  gets(nazwiska[i]);
+  tablica[0][i+1]=i;
+  printf("\nPodaj mi Wielkosc lokaty %s", nazwiska[i]);
+  scanf("%d", &tablica[1][i+1]);
+  printf("\nPodaj mi Dzien zalozenia lokaty %s", nazwiska[i]);
+  scanf("%d", &tablica[2][i+1]);
+  printf("\nPodaj mi Miesiac zalozenia lokaty %s", nazwiska[i]);
+  scanf("%d", &tablica[3][i+1]);
+  printf("\nPodaj Rok zalozenia tablicy %s", nazwiska[i]);
+  scanf("%d", &tablica[4][i+1]);
+  }
+  
+  for(i=0;i<=5;i++)
+  printf(" %s ", tytuly[i] );
+  printf("\n");
+  for(i=0;i<ROZMIAR;i++)
+  printf(" %s  %d  %d  %d  %d \n", nazwiska[ tablica[0][i+1] ], tablica[1][i+1], tablica[2][i+1], tablica[3][i+1], tablica[4][i+1]);
+ 
+          
+            
+
+
+return 0;
+}
+
