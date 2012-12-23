ecd51b58262d4fb3c921e5824639c524cc30e485
Piotr <piotr@piotr>
2012-12-23 15:06:21 +0100

    dziala przepisanie miejscowosci do list, troszke nieladne ale dziala

--- hurtownia2.c
+++ hurtownia2.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-/*Niestety coś chrzani się przy wskaźniku do pierwszego elementu listy.. jest to conajmniej dziwne to co się dzieje. Wygląda to tak jakby za każdym przejściem pętli while wykonywała się instrukcja head_g=current_g ale to występuje tylko w headzie, a sprawdzałem i tam program wchodzi tylko raz... */
+/*Funkcja przepisz_miejscowosci przepisuje główne i pobocnze miasta do osobnych list, każde głowne miasto ma wskaźnik do pierwszego i ostatniego miasta pobocznego */
 
 
 
@@ -66,71 +66,93 @@ printf("Do widzenia\n");
 
 struktura3 przepisz_miejscowosci(struktura1 *head_g,struktura2 *head_p, struktura3 head , FILE *miejscowosci)
 {
-  char temp[255],*result;
+  int i=0;/* i jest mi potrzebne do ustalania myheada dla pierwszego */
+  char temp[255],buff[255],*result;
   struktura1 *current_g, *previous_g;
   struktura2 *current_p, *previous_p;
-
-  
+  head_g=NULL;  
 
   rewind(miejscowosci); /*profilaktyki nigdy za duzo*/
   current_g=(struktura1 *)malloc(sizeof(struktura1));
   current_p=(struktura2 *)malloc(sizeof(struktura2));
   
+  /* head_g=current_g;*/
   while(result!=NULL) /*może EOF? */
     { 
-      result = fgets(temp,100,miejscowosci);
-      if(result!=NULL)
-	{
-	  if((temp[0]<=100 && temp[0]>=65) || (temp[0]<=122 && temp[0]>=97))
+          result = fgets(temp,100,miejscowosci);
+      
+	  if((temp[0]<=90 && temp[0]>=65) || (temp[0]<=122 && temp[0]>=97))
 	    {
 	     if(head_g==NULL)
 	       {
-	       head_g=current_g;
-	       current_g->myhead=current_p;
-	       printf("say what again!");
-	       }
-	     else
-	       {
-		 previous_g=current_g; /*dałem to tutaj ze względu na liste pobocznych, do tej linijki cały czas pracuje na currentcie */
-	        previous_g->next_m=current_g;
-                current_g->myhead=previous_p->next_p; /*head dla każdego pobocznego*/
-	       printf("%s",current_g->miasto_g);
-	       printf("%s", previous_g->miasto_g);
-	       printf("2%s",head_g->miasto_g);
+		 head_g=current_g;
+		 /*head do "najpierwszego" ↓ */
+		 current_g->myhead=current_p;
+		 i=0;
                }
+	     else
+	       {		 	 
+                 current_g=(struktura1 *)malloc(sizeof(struktura1));
+		 previous_g->next_m=current_g;                  
+	       }
 	     current_g->next_m=NULL;
-	     current_g->miasto_g=(char *)malloc(sizeof(char)*strlen(temp)+1);
+	     /*instrukcje*/
+	     current_g->miasto_g=(char*)malloc(sizeof(char)*strlen(temp)+1);
 	     strcpy(current_g->miasto_g,temp);
-	          	    
+
+	     previous_g=current_g;         
+             i=1;	        	    
 	    }
 	  else
 	    {
-	      if(head_p==NULL)
-		head_p=current_p; 
-              else
-		previous_p->next_p=current_p;
-
-	      current_p->next_p=NULL;
-	      current_p->miasto_p=(char*)malloc(sizeof(char)*strlen(temp)+1);
-	      strcpy(current_p->miasto_p,temp);
-	      /*printf("%s",current_p->miasto_p);*/
-              current_g->mytail=current_p; /* zapisuje zawsze aż do zmiany| OK */                 
-	      previous_p=current_p;
-         
+	     if(head_p==NULL)
+	       {
+		 head_p=current_p;
+		 i=0;
+	       }
+	     else
+	       {
+                 current_p=(struktura2 *)malloc(sizeof(struktura2));	
+	         previous_p->next_p=current_p;
+		 if(i==1)
+		   {
+		     current_g->myhead=current_p;
+		     i=0;
+		   }
+               }    
+	     current_p->next_p=NULL;
+	     /*instrukcje*/
+	     /*%[A-Za-z ] wczytuje tylko te określone znaki i spacje dzięki temu moge pobrac 2 słowa jako jeden string*/
+	     sscanf(temp, "%d %[A-Za-z ] \n",&current_p->odleglosc,buff);
+	     current_p->miasto_p=(char*)malloc(sizeof(char)*strlen(buff)+1);
+	     strcpy(current_p->miasto_p,buff);
+	     
+             /*koniec pobocznych od głównego to ↓ */
+             current_g->mytail=current_p;
+	     previous_p=current_p;
 	    }
    
-	}
-
     }
+  current_p=head_p;
+  current_g=head_g;
   
-  
-    /*
   while(current_g!=NULL)
     {
       printf("%s",current_g->miasto_g);
+      printf("Myhead: %s, %d\n",current_g->myhead->miasto_p,current_g->myhead->odleglosc);
+      printf("Mytail: %s  %d\n",current_g->mytail->miasto_p,current_g->mytail->odleglosc);
+ current_p=current_g->myhead;
+       
+      /*
+      do
+	{
+	  printf("-%s - %d\n",current_p->miasto_p, current_p->odleglosc);
+	  current_p=current_p->next_p;
+	}while(current_p!=current_g->mytail);
+      */
       current_g=current_g->next_m;
-    }
-  */
+      }
   
+    
 return head;
 }
