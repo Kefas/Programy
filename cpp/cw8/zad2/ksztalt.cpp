22ab651f8ebfc018363804368a9a891d153689cd
Piotr Konsek <piokon@mops.uci.agh.edu.pl>
2013-04-08 13:51:10 +0200

    up

--- cpp/cw8/zad2/ksztalt.cpp
+++ cpp/cw8/zad2/ksztalt.cpp
@@ -6,14 +6,17 @@ using namespace std;
 void Kolo::rysuj()
 {
   cout << "Kolo o promieniu "<< this->r<<" i wspolrzednych (" << this->x<<";"<<this->y <<")" << endl;
+  cout << "  __  "<< endl << " /  \\ " <<endl << " |   |"<< endl << " \\__/ " << endl;
 }
 
 void Trojkat::rysuj()
 {
   cout << "Trojkat o dlugosci boku"<< this->a<<" i wspolrzednych (" << this->x<<";"<<this->y <<")" << endl;
+  cout << " ^ " << endl << "/_\\" <<endl;
 }
 
 void Kwadrat::rysuj()
 {
   cout << "Kwadrat o dlugosci boku "<<this->a<<" i wspolrzednych (" << this->x<<";"<<this->y <<")" << endl;
+  cout << " _ " << endl << "|_|" <<endl;
 }
