bd82b9919acecfbe78b23de231cb48fe7b85e0dc
Piotr Konsek <piotr@piotr-ThinkPad-Edge-E530.(none)>
2013-07-22 18:15:57 +0200

    up

--- html/codecademy/form_validation/script.js
+++ html/codecademy/form_validation/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     
     $("#header").animate({
@@ -11,9 +10,25 @@ $(document).ready(function(){
     $("#formularz").fadeTo(600, 0.01);
     $("#formularz").fadeTo(500, 1);
     $("#submit").click(function(){
-	var x = $("#first").val();
-	console.log(x);
-	alert(x);
+	var first = $("#first").val();
+	var last = $("#last").val();
+	var date = $("#date").val();
+	var man = $("#man:checked").val();
+	var woman = $("#woman:checked").val();
+	
+	if( first.length === 0 ){
+	    alert("Nie wpisałeś imienia!");
+	}
+	if( last.length === 0){
+	    alert("brak nazwiska");
+	}
+	if( man === "on" && woman === "on" ){
+	    alert("Nie mozesz byc jednoczesnie baba i chopem!");
+	}
+	if( man !== "on" && woman !=="on"){
+	    alert("bezplciowiec co?? ");
+	}
+	alert(man);
     });
 });
 
