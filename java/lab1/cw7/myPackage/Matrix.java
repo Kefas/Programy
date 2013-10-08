a44eb648dad7b5b18c53609b7519ab56ac1a3eb4
Piotr Konsek <piokon@mops.uci.agh.edu.pl>
2013-10-08 15:26:04 +0200

    up java

---
+++ java/lab1/cw7/myPackage/Matrix.java
@@ -0,0 +1,14 @@
+package MyPackage;
+import java.util.*;
+
+public class Matrix{
+    private rows;
+    private cols;
+
+    public static void main(String [] argv){
+	Matrix m = new Matrix{3,3};
+    }
+    
+    public static int add(int A, int B){
+	
+    
